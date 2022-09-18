04ae02ac56133ebd53db8f911653d702820059f4
h454-pixel <58342851+h454-pixel@users.noreply.github.com>
2022-09-18 11:21:35 +0530

    Update MainActivity.java
    pull commit

--- app/src/main/java/com/example/gitpractice/MainActivity.java
+++ app/src/main/java/com/example/gitpractice/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //////////////latest changes upadate free
+        
+        /// pull request first branch
     }
 }
